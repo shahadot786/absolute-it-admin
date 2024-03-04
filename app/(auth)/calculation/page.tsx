c76a8207b8938ea0d54fa7da04e2000b0801b070
shahadot786 <shrhossain786@gmail.com>
2024-03-04 12:43:37 +0600

    discount calculator added

---
+++ app/(auth)/calculation/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import React, { useState } from "react";
+
+const Calculation: React.FC = () => {
+  const [amount, setAmount] = useState<number>(0);
+  const [discount, setDiscount] = useState<number>(0);
+  const [result, setResult] = useState<number | null>(null);
+
+  const calculate = (amount: number, discount: number): number | null => {
+    if (discount >= 100) return null; // Prevent division by zero or negative discounts
+    return (amount * 100) / (100 - discount);
+  };
+
+  const handleCalculate = () => {
+    const calculatedResult = calculate(amount, discount);
+    setResult(calculatedResult);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        height: "100vh",
+        marginTop: "50px",
+      }}
+    >
+      <div>
+        <h1 className="text-5xl font-bold my-4">Discount Calculator</h1>
+        <div>
+          <div>
+            <label htmlFor="amount" className="text-lg font-bold my-2">
+              Amount
+            </label>
+          </div>
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={(e) => setAmount(parseFloat(e.target.value))}
+            className="border-2 border-gray-800 rounded-sm  my-2"
+          />
+        </div>
+        <div>
+          <div>
+            <label htmlFor="discount" className="text-lg font-bold my-2">
+              Discount (%)
+            </label>
+          </div>
+          <input
+            type="number"
+            id="discount"
+            value={discount}
+            onChange={(e) => setDiscount(parseFloat(e.target.value))}
+            className="border-2 border-gray-800 rounded-sm  my-2"
+          />
+        </div>
+        <button
+          onClick={handleCalculate}
+          className="mt-5 flex w-full justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500"
+        >
+          Calculate
+        </button>
+        {result !== null && (
+          <p className="text-lg font-bold my-2">
+            Original Amount: {result.toFixed(0)}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Calculation;
